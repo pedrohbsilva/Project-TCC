f9a277e3badb38e1a8abff04f979060e4e4a25be
pedro <phbs1235@gmail.com>
2021-08-28 22:02:29 -0300

    update interfaces and add new functions in graphics

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,9 +17,10 @@ import {
   DrawerStyleProps,
 } from '../../interfaces';
 import api from '../../utils/api';
-import { Container, ListOptions } from './styles';
+import { Container, ListOptions, TextHome, ViewHome } from './styles';
 import HeaderComponent from '../../components/Header';
 import LastDaysGraphic from '../../components/LastDaysGraphic';
+import LastMonthGraphic from '../../components/LastMonthGraphic';
 
 const Home = ({
   drawerAnimationStyle,
@@ -27,36 +28,46 @@ const Home = ({
   const { user } = useAuth();
   const [RTItems, setRTItems] = useState<RTPagination[]>([]);
   const [lastDaysItems, setLastDaysItems] = useState<LastDaysProps[]>([]);
+  const [lastMonthItems, setLastMonthItems] = useState<LastDaysProps[]>([]);
+  const [allGenerated, setAllGenerated] = useState<number>();
+  const [currentDate, setCurrentDate] = useState('');
   const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = React.useState(false);
 
   const wait = (timeout: number): Promise<void> => {
     return new Promise(resolve => setTimeout(resolve, timeout));
   };
-
   const onRefresh = useCallback(() => {
     setRefreshing(true);
+    setCurrentDate(new Date().toLocaleTimeString('PT-BR').substring(0, 8));
     wait(2000).then(() => setRefreshing(false));
   }, []);
+
+  const paramsObject = {
+    year: new Date().getFullYear(),
+    month: new Date().getMonth() + 1,
+    day: new Date().getDate(),
+    usinaId: user.cliente.usinas[0],
+  };
 
   useEffect(() => {
     const load = async () => {
       try {
         const responseRT = await api.get(`/api/geracaoRt/getByDate`, {
-          params: {
-            year: new Date().getFullYear(),
-            month: new Date().getMonth() + 1,
-            day: new Date().getDate(),
-            usinaId: user.cliente.usinas[0],
-          },
+          params: paramsObject,
         });
         const responseLD = await api.get(`/api/geracao/getLastDays`, {
           params: {
-            daysBefore: 7,
+            daysBefore: 30,
             usinaId: user.cliente.usinas[0],
           },
         });
-        setLastDaysItems(responseLD.data);
+        const responseAllGenerated = await api.get('/api/geracao/getByDate', {
+          params: paramsObject,
+        });
+        setLastDaysItems(responseLD.data.splice(0, 24));
+        setLastMonthItems(responseLD.data);
+        setAllGenerated(responseAllGenerated.data.totalGerado / 1000);
         setRTItems(responseRT.data);
         setLoading(false);
       } catch (error) {
@@ -91,6 +102,11 @@ const Home = ({
             >
               <HeaderComponent />
               <Container>
+                {currentDate !== '' && (
+                  <ViewHome>
+                    <TextHome>A última atualização foi: {currentDate}</TextHome>
+                  </ViewHome>
+                )}
                 {loading ? (
                   <View style={{ flex: 1, padding: 20 }}>
                     <ActivityIndicator />
@@ -102,6 +118,10 @@ const Home = ({
                     activeDotColor="#61cf7e"
                   >
                     {RTItems.length > 0 && <RealTimeGraphic data={RTItems} />}
+
+                    {lastMonthItems.length > 0 && (
+                      <LastMonthGraphic data={lastMonthItems} />
+                    )}
                     {lastDaysItems.length > 0 && (
                       <LastDaysGraphic data={lastDaysItems} />
                     )}
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -10,3 +10,15 @@ export const ListOptions = styled.ScrollView.attrs({
 })`
   flex: 1;
 `;
+
+export const TextHome = styled.Text`
+  color: #61cf71;
+  font-size: 14px;
+  font-family: 'Poppins_600SemiBold';
+  text-align: left;
+`;
+
+export const ViewHome = styled.View`
+  width: 100%;
+  padding: 0 20px 0 20px;
+`;
